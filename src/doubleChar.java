774bfed8afa7e401bc414e4d38d763ba056dcc6d
Rlong1218 <7dbrobert1218@gmail.com>
2020-07-18 15:52:38 -0600

    Java practice problems day 1

---
+++ src/doubleChar.java
@@ -0,0 +1,11 @@
+//Given a string, return a string where for every char in the original, there are two chars.
+
+public class doubleChar {
+  public String doubleChar(String str) {
+    String doubled = "";
+    for (int i = 0; i <str.length() ; i++) {
+      doubled = doubled + str.charAt(i) + str.charAt(i);
+    }
+    return doubled;
+  }
+}
---
+++ src/makeBricks.java
@@ -0,0 +1,16 @@
+//We want to make a row of bricks that is goal inches long.
+// We have a number of small bricks (1 inch each) and big bricks (5 inches each).
+// Return true if it is possible to make the goal by choosing from the given bricks.
+
+public class makeBricks {
+
+  public boolean makeBricks(int small, int big, int goal) {
+    if (small + big * 5 == goal || small >= goal || (goal / 5 <= big && goal % 5 == 0)) {
+      return true;
+    }
+    else if (goal < big * 5) {
+      return small >= goal % 5;
+    }
+    else return (goal - big * 5) <= small;
+  }
+}
